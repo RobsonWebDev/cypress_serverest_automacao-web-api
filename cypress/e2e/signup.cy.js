5c033c9784e0ec6c02e49f17e47773fac343d14f
robsoncerqueira <robsonwebpro@gmail.com>
2024-06-20 13:21:08 -0300

    Cadastro com Sucesso

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -25,12 +25,15 @@ describe('Criar Usuario', () => {
     cy.get('input[data-testid=email]').type(user.email)
 
     cy.get('input[data-testid=password]').type(user.password)
-    
+
     cy.contains('label[for=administrador]', 'Cadastrar como administrador?')
       .parent()
       .get('input[data-testid=checkbox]')
       .click()
 
+    cy.contains('button[data-testid=cadastrar]', 'Cadastrar').click()
 
+    cy.contains('.alert-link', 'Cadastro realizado com sucesso')
+      .should('be.visible')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,4 @@
-Cypress.Commands.add('addUser', (user) => {
+Cypress.Commands.add('postUser', (user) => {
     cy.request({
         url: '/usuarios',
         method: 'POST',
@@ -6,3 +6,28 @@ Cypress.Commands.add('addUser', (user) => {
         failOnStatusCode: false
     });
 });
+
+Cypress.Commands.add('login', (user) => {
+
+    delete user.name
+    delete user.administrador
+
+    cy.request({
+        url: '/login',
+        method: 'POST',
+        body: user,
+        failOnStatusCode: false
+    });
+});
+
+
+Cypress.Commands.add('getUser', (user) => {
+    cy.request({
+        url: '/usuarios',
+        method: 'GET',
+        body: user,
+        failOnStatusCode: false
+    });
+});
+
+Cypress.Commands
